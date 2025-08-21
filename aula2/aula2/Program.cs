6ea52e67ca71816bdc00ddbc291eef487a5fd453
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2025-08-20 21:44:21 -0300

    Realizando exercicios da faculdade

---
+++ aula2/aula2/Program.cs
@@ -0,0 +1,50 @@
+﻿
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Console.WriteLine("Digite um numero: ");
+            // string entrada = Console.ReadLine();
+            // int numero;
+
+            // if (int.TryParse(entrada, out numero))
+            // {
+            //     Console.WriteLine("Você digitou: " + numero);
+
+            //     if (numero % 2 == 0)
+            //     {
+            //         Console.WriteLine("O numero é par");
+
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("O numero não é par");
+
+            //     }
+
+
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Entrada inválida! Digite um número inteiro.");
+            //     return;
+            // }
+
+
+            Console.WriteLine("Digite uma temperatura em celsius: ");
+            string entrada = Console.ReadLine();
+            double temp;
+
+            if (double.TryParse(entrada, out temp))
+            {
+                double valorFinal = (temp * 9 / 5) + 32;
+                Console.WriteLine(valorFinal);
+            }
+           
+        }
+            
+
+        }
+    }
---
+++ aula2/exercicio.md
@@ -0,0 +1,2 @@
+[x] Ler um numero inteiro e informe se é par ou impar 
+[] Ler uma temperatura em C e converter para F
